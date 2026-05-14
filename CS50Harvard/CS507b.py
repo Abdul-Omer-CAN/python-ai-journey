3a28db9aa5b8ff500806859bbbef60c65a182520
Abdulrehman Omer <abdulrehman.omer001@gmail.com>
2026-05-14 18:44:03 -0400

    Remove CS50 Files moved to seperate repo

--- CS50Harvard/CS507b.py
+++
@@ -1,5 +0,0 @@
-with open("student.csv") as file:
-    for line in file:
-        row = line.rstrip().split(",")
-        if len(row) > 1:          # if there is more than one item in the row then print it if not then dont print it. items are seperated by ','
-            print(f"{row[0]} is in {row[1]}")
--- CS50Harvard/CS507e.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-name = input("Whats your name? ")
-home = input("Where is your home? ")
-
-with open("student3.csv", "a", newline="") as file:
-    writer = csv.DictWriter(file, fieldnames=["name", "home"])
-    writer.writerow({"home": home, "name": name})
-s
